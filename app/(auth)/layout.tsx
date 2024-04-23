c719055deb5aad41f3e083397b0639b947774071
A-TECH <atech2021715@gmail.com>
2024-04-23 13:20:43 +0300

    first commit

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -7,8 +7,8 @@ import { ClerkProvider } from "@clerk/nextjs";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Borcelle - Admin Auth",
-  description: "Admin dashboard to manage Borcelle's data",
+  title: "ButterFly - Admin Auth",
+  description: "Admin dashboard to manage ButterFly's data",
 };
 
 export default function RootLayout({
